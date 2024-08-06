f87eeee47616855f41cd738525f2c0649d1e1614
NaveenNeeratti <neeratinaveen675@gmail.com>
2024-08-06 09:17:58 +0530

    QEats backend

---
+++ qeatsbackend/src/main/java/com/crio/qeats/exchanges/GetRestaurantsRequest.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.exchanges;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// TODO: CRIO_TASK_MODULE_RESTAURANTSAPI
+//  Implement GetRestaurantsRequest.
+//  Complete the class such that it is able to deserialize the incoming query params from
+//  REST API clients.
+//  For instance, if a REST client calls API
+//  /qeats/v1/restaurants?latitude=28.4900591&longitude=77.536386&searchFor=tamil,
+//  this class should be able to deserialize lat/long and optional searchFor from that.
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GetRestaurantsRequest {
+public GetRestaurantsRequest(double latitude, double longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
+@NotNull
+private Double latitude;
+@NotNull
+private Double longitude;
+private String searchFor;
+}
+
